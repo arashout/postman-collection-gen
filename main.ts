0a075d0ae8d97c66f9f09ba19756159af2f4f9df
Arash Outadi <arash.out@gmail.com>
2020-03-07 01:06:23 -0800

    Added language pairs

--- main.ts
+++ main.ts
@@ -6,40 +6,49 @@ import { readFileSync } from 'fs';
 import * as commander from 'commander';
 
 const languageVariantPairs = [
-    ["Language", "Variant"],
-    ["C#", "RestSharp"],
-    ["cURL", "cURL"],
-    ["Go", "Native"],
-    ["HTTP", "HTTP"],
-    ["Java", "OkHttp"],
-    ["Java", "Unirest"],
-    ["JavaScript", "Fetch"],
-    ["JavaScript", "jQuery"],
-    ["JavaScript", "XHR"],
-    ["NodeJs", "Native"],
-    ["NodeJs", "Request"],
-    ["NodeJs", "Unirest"],
-    ["Objective-C", "NSURLSession"],
-    ["OCaml", "Cohttp"],
-    ["PHP", "cURL"],
-    ["PHP", "pecl_http"],
-    ["PHP", "HTTP_Request2"],
-    ["PowerShell", "RestMethod"],
-    ["Ruby", "Net:HTTP"],
-    ["Shell", "Httpie"],
-    ["Shell", "wget"],
-    ["Swift", "URLSession"],
-];
+    "C#,RestSharp",
+    "cURL,cURL",
+    "Go,Native",
+    "HTTP,HTTP",
+    "Java,OkHttp",
+    "Java,Unirest",
+    "JavaScript,Fetch",
+    "JavaScript,jQuery",
+    "JavaScript,XHR",
+    "NodeJs,Native",
+    "NodeJs,Request",
+    "NodeJs,Unirest",
+    "Objective-C,NSURLSession",
+    "OCaml,Cohttp",
+    "PHP,cURL",
+    "PHP,pecl_http",
+    "PHP,HTTP_Request2",
+    "PowerShell,RestMethod",
+    "Ruby,Net:HTTP",
+    "Shell,Httpie",
+    "Shell,wget",
+    "Swift,URLSession",
+].map(v => v.toLowerCase());
+
+function parseTuple(value: string, dummy: string): { language: string, variant: string } {
+    const v = value.trim().toLowerCase();
+    if (!languageVariantPairs.includes(v)) {
+        throw `Not a valid <langauge,variant> pair.Try one of:\n${languageVariantPairs.join('\n')}\n`;
+    }
+
+    const tuple = v.split(',');
+
+    console.assert(tuple.length === 2);
+    return { language: tuple[0], variant: tuple[1] };
+}
 
 const program = new commander.Command('generate');
 program.version('0.1');
 
 program
     .requiredOption('-c, --collection <path>', 'Path to the Postman 2.1 Collection JSON')
-    .option('-l,--language <string>', 'Language to output', 'cURL')
-    .option('-v,--variant <string>', 'Variant of language to output', 'cURL')
-    .option('-e,--envvars <path>', `Path to environment variables exported from Postman.
-    NOTE: Environment variables will not override variables provided in collection`)
+    .option('-l,--language_variant <tuple>', 'Language,Variant pair to output', parseTuple, 'cURL,cURL')
+    .option('-e,--envvars <path>', `Path to environment variables exported from Postman. NOTE: Environment variables will not override variables provided in collection`)
     .option('-d, --debug', 'Output additional debugging info');
 
 program.parse(process.argv);
@@ -58,12 +67,10 @@ const collection = new Collection(JSON.parse(readFileSync(collectionPath).toStri
 
 debugPrint(collection);
 
-const language = program['language'],
-    variant = program['variant'],
-    options = {
-        trimRequestBody: true,
-        followRedirect: true
-    };
+const options = {
+    trimRequestBody: true,
+    followRedirect: true
+};
 
 
 function isItem(itemG: Item | ItemGroup<Item>): itemG is Item {
@@ -79,11 +86,12 @@ if (program['envvars']) {
     });
 }
 
-debugPrint(environmentVariables);
+const lvp: { language: string, variant: string } = program['language_variant'];
 
+debugPrint(environmentVariables);
 collection.items.all().forEach(item => {
     if (isItem(item))
-        codegen.convert(language, variant, item.request, options, function (error: any, snippet: any) {
+        codegen.convert(lvp.language, lvp.variant, item.request, options, function (error: any, snippet: any) {
             if (error) {
                 console.error('Error trying to generate code for request:', item.request, error);
             }
