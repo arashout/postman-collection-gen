54f1f9485d7c24108d626ca50a0cc74e43ae0f24
Massi <57674329+MassimoTambu@users.noreply.github.com>
2023-02-28 01:30:47 +0100

    FEAT & FIX: Scrolling through folders, fixed env vars not substituted, added Dart… (#6)
    * Scrolling through folders, fixed env vars not substituted, added Dart language support and updated packages
    
    * Fixed type
    
    * Updated main.js
    
    ---------
    
    Co-authored-by: Massimo Tamburini <massimo.tamburini@botika.ai>

--- main.js
+++ main.js
@@ -5,28 +5,31 @@ var codegen = require('postman-code-generators');
 var fs_1 = require("fs");
 var commander = require("commander");
 var languageVariantPairs = [
-    "C#,RestSharp",
-    "cURL,cURL",
-    "Go,Native",
-    "HTTP,HTTP",
-    "Java,OkHttp",
-    "Java,Unirest",
-    "JavaScript,Fetch",
-    "JavaScript,jQuery",
-    "JavaScript,XHR",
-    "NodeJs,Native",
-    "NodeJs,Request",
-    "NodeJs,Unirest",
-    "Objective-C,NSURLSession",
-    "OCaml,Cohttp",
-    "PHP,cURL",
-    "PHP,pecl_http",
-    "PHP,HTTP_Request2",
-    "PowerShell,RestMethod",
-    "Ruby,Net:HTTP",
-    "Shell,Httpie",
-    "Shell,wget",
-    "Swift,URLSession",
+    'C#,RestSharp',
+    'Dart,http',
+    'cURL,cURL',
+    'Go,Native',
+    'HTTP,HTTP',
+    'Java,OkHttp',
+    'Java,Unirest',
+    'JavaScript,Fetch',
+    'JavaScript,jQuery',
+    'JavaScript,XHR',
+    'NodeJs,Native',
+    'NodeJs,Request',
+    'NodeJs,Unirest',
+    'Objective-C,NSURLSession',
+    'OCaml,Cohttp',
+    'PHP,cURL',
+    'PHP,pecl_http',
+    'PHP,HTTP_Request2',
+    'PowerShell,RestMethod',
+    'Python,http.client',
+    'Python,Requests',
+    'Ruby,Net:HTTP',
+    'Shell,Httpie',
+    'Shell,wget',
+    'Swift,URLSession'
 ].map(function (v) { return v.toLowerCase(); });
 function parseTuple(value, dummy) {
     var v = value.trim().toLowerCase();
@@ -38,7 +41,7 @@ function parseTuple(value, dummy) {
     return { language: tuple[0], variant: tuple[1] };
 }
 var program = new commander.Command('generate');
-program.version('0.1');
+program.version('0.2');
 program
     .requiredOption('-c, --collection <path>', 'Path to the Postman 2.1 Collection JSON')
     .option('-l,--language_variant <tuple>', 'Language,Variant pair to output', parseTuple, { language: 'curl', variant: 'curl' })
@@ -52,7 +55,7 @@ function debugPrint(message) {
 }
 debugPrint(program.opts());
 var collectionPath = program['collection'];
-var collection = new postman_collection_1.Collection(JSON.parse(fs_1.readFileSync(collectionPath).toString()));
+var collection = new postman_collection_1.Collection(JSON.parse((0, fs_1.readFileSync)(collectionPath).toString()));
 debugPrint(collection);
 var options = {
     trimRequestBody: true,
@@ -61,23 +64,16 @@ var options = {
 function isItem(itemG) {
     return itemG.request !== undefined;
 }
-var environmentVariables = new postman_collection_1.VariableList(new postman_collection_1.Property({ name: 'environmentVariables' }), []);
-if (program['envvars']) {
-    var environment = JSON.parse(fs_1.readFileSync(program['envvars']).toString());
-    debugPrint(environment);
-    environment['values'].forEach(function (v) {
-        environmentVariables.append(new postman_collection_1.Variable(v));
-    });
+function isItemGroup(itemG) {
+    return itemG.items !== undefined;
 }
-var lvp = program['language_variant'];
-debugPrint(environmentVariables);
-collection.items.all().forEach(function (item) {
-    if (isItem(item))
+function printSnippet(item) {
+    if (isItem(item)) {
         codegen.convert(lvp.language, lvp.variant, item.request, options, function (error, snippet) {
             if (error) {
                 console.error('Error trying to generate code for request:', item.request, error);
             }
-            var completeSnippet = collection.variables.replace(snippet);
+            var completeSnippet = collection.variables.replace(snippet, environmentVariables);
             var re = /(?:\{\{(.+?)\}\})/g;
             var matches = re.exec(completeSnippet);
             if (matches && matches.length > 0) {
@@ -85,4 +81,19 @@ collection.items.all().forEach(function (item) {
             }
             console.log(completeSnippet);
         });
-});
+    }
+    else if (isItemGroup(item)) {
+        item.items.all().forEach(printSnippet);
+    }
+}
+var environmentVariables = new postman_collection_1.VariableList(new postman_collection_1.Property({ name: 'environmentVariables' }), []);
+if (program['envvars']) {
+    var environment = JSON.parse((0, fs_1.readFileSync)(program['envvars']).toString());
+    debugPrint(environment);
+    environment['values'].forEach(function (v) {
+        environmentVariables.append(new postman_collection_1.Variable(v));
+    });
+}
+var lvp = program['language_variant'];
+debugPrint(environmentVariables);
+collection.items.all().forEach(printSnippet);
--- main.ts
+++ main.ts
@@ -1,107 +1,155 @@
-import { Collection, Item, ItemGroup, VariableList, Property, Variable } from 'postman-collection';
-const codegen = require('postman-code-generators')
+import {
+  Collection,
+  Item,
+  ItemGroup,
+  VariableList,
+  Property,
+  Variable
+} from 'postman-collection';
+const codegen = require('postman-code-generators');
 
 import { readFileSync } from 'fs';
 
 import * as commander from 'commander';
 
 const languageVariantPairs = [
-    "C#,RestSharp",
-    "cURL,cURL",
-    "Go,Native",
-    "HTTP,HTTP",
-    "Java,OkHttp",
-    "Java,Unirest",
-    "JavaScript,Fetch",
-    "JavaScript,jQuery",
-    "JavaScript,XHR",
-    "NodeJs,Native",
-    "NodeJs,Request",
-    "NodeJs,Unirest",
-    "Objective-C,NSURLSession",
-    "OCaml,Cohttp",
-    "PHP,cURL",
-    "PHP,pecl_http",
-    "PHP,HTTP_Request2",
-    "PowerShell,RestMethod",
-    "Python,http.client",
-    "Python,Requests",
-    "Ruby,Net:HTTP",
-    "Shell,Httpie",
-    "Shell,wget",
-    "Swift,URLSession",
+  'C#,RestSharp',
+  'Dart,http',
+  'cURL,cURL',
+  'Go,Native',
+  'HTTP,HTTP',
+  'Java,OkHttp',
+  'Java,Unirest',
+  'JavaScript,Fetch',
+  'JavaScript,jQuery',
+  'JavaScript,XHR',
+  'NodeJs,Native',
+  'NodeJs,Request',
+  'NodeJs,Unirest',
+  'Objective-C,NSURLSession',
+  'OCaml,Cohttp',
+  'PHP,cURL',
+  'PHP,pecl_http',
+  'PHP,HTTP_Request2',
+  'PowerShell,RestMethod',
+  'Python,http.client',
+  'Python,Requests',
+  'Ruby,Net:HTTP',
+  'Shell,Httpie',
+  'Shell,wget',
+  'Swift,URLSession'
 ].map(v => v.toLowerCase());
 
-function parseTuple(value: string, dummy: string): { language: string, variant: string } {
-    const v = value.trim().toLowerCase();
-    if (!languageVariantPairs.includes(v)) {
-        throw `Not a valid <langauge,variant> pair.Try one of:\n${languageVariantPairs.join('\n')}\n`;
-    }
+function parseTuple(
+  value: string,
+  dummy: string
+): { language: string; variant: string } {
+  const v = value.trim().toLowerCase();
+  if (!languageVariantPairs.includes(v)) {
+    throw `Not a valid <langauge,variant> pair.Try one of:\n${languageVariantPairs.join(
+      '\n'
+    )}\n`;
+  }
 
-    const tuple = v.split(',');
+  const tuple = v.split(',');
 
-    console.assert(tuple.length === 2);
-    return { language: tuple[0], variant: tuple[1] };
+  console.assert(tuple.length === 2);
+  return { language: tuple[0], variant: tuple[1] };
 }
 
 const program = new commander.Command('generate');
-program.version('0.1');
+program.version('0.2');
 
 program
-    .requiredOption('-c, --collection <path>', 'Path to the Postman 2.1 Collection JSON')
-    .option('-l,--language_variant <tuple>', 'Language,Variant pair to output', parseTuple, { language: 'curl', variant: 'curl' })
-    .option('-e,--envvars <path>', `Path to environment variables exported from Postman. NOTE: Environment variables will not override variables provided in collection`)
-    .option('-d, --debug', 'Output additional debugging info');
+  .requiredOption(
+    '-c, --collection <path>',
+    'Path to the Postman 2.1 Collection JSON'
+  )
+  .option(
+    '-l,--language_variant <tuple>',
+    'Language,Variant pair to output',
+    parseTuple,
+    { language: 'curl', variant: 'curl' }
+  )
+  .option(
+    '-e,--envvars <path>',
+    `Path to environment variables exported from Postman. NOTE: Environment variables will not override variables provided in collection`
+  )
+  .option('-d, --debug', 'Output additional debugging info');
 
 program.parse(process.argv);
 
 function debugPrint(message: any) {
-    if (program.debug) {
-        console.log('DEBUG:', message);
-    }
+  if (program.debug) {
+    console.log('DEBUG:', message);
+  }
 }
 
 debugPrint(program.opts());
 
-const collectionPath: string = program['collection']
+const collectionPath: string = program['collection'];
 
-const collection = new Collection(JSON.parse(readFileSync(collectionPath).toString()));
+const collection = new Collection(
+  JSON.parse(readFileSync(collectionPath).toString())
+);
 
 debugPrint(collection);
 
 const options = {
-    trimRequestBody: true,
-    followRedirect: true
+  trimRequestBody: true,
+  followRedirect: true
 };
 
-
 function isItem(itemG: Item | ItemGroup<Item>): itemG is Item {
-    return (<Item>itemG).request !== undefined;
+  return (<Item>itemG).request !== undefined;
 }
 
-let environmentVariables = new VariableList(new Property({ name: 'environmentVariables' }), []);
-if (program['envvars']) {
-    const environment = JSON.parse(readFileSync(program['envvars']).toString())
-    debugPrint(environment);
-    environment['values'].forEach((v: { value: string, key: string }) => {
-        environmentVariables.append(new Variable(v));
+function isItemGroup(itemG: Item | ItemGroup<Item>): itemG is ItemGroup<Item> {
+  return (<ItemGroup<Item>>itemG).items !== undefined;
+}
+
+function printSnippet(item: Item | ItemGroup<Item>) {
+  if (isItem(item)) {
+    codegen.convert(lvp.language, lvp.variant, item.request, options, function(
+      error: any,
+      snippet: any
+    ) {
+      if (error) {
+        console.error(
+          'Error trying to generate code for request:',
+          item.request,
+          error
+        );
+      }
+      const completeSnippet = collection.variables.replace(
+        snippet,
+        environmentVariables
+      );
+      const re = /(?:\{\{(.+?)\}\})/g;
+      const matches = re.exec(completeSnippet);
+      if (matches && matches.length > 0) {
+        matches.forEach(m => console.warn(`${m} : Variable not provided`));
+      }
+      console.log(completeSnippet);
     });
+  } else if (isItemGroup(item)) {
+    item.items.all().forEach(printSnippet);
+  }
 }
 
-const lvp: { language: string, variant: string } = program['language_variant'];
+const environmentVariables = new VariableList(
+  new Property({ name: 'environmentVariables' }),
+  []
+);
+if (program['envvars']) {
+  const environment = JSON.parse(readFileSync(program['envvars']).toString());
+  debugPrint(environment);
+  environment['values'].forEach((v: { value: string; key: string }) => {
+    environmentVariables.append(new Variable(v));
+  });
+}
+
+const lvp: { language: string; variant: string } = program['language_variant'];
 debugPrint(environmentVariables);
-collection.items.all().forEach(item => {
-    if (isItem(item))
-        codegen.convert(lvp.language, lvp.variant, item.request, options, function (error: any, snippet: any) {
-            if (error) {
-                console.error('Error trying to generate code for request:', item.request, error);
-            }
-            const completeSnippet = collection.variables.replace(snippet)
-            const re = /(?:\{\{(.+?)\}\})/g
-            const matches = re.exec(completeSnippet);
-            if (matches && matches.length > 0) {
-                matches.forEach(m => console.warn(`${m} : Variable not provided`));
-            }
-            console.log(completeSnippet);
-        });
-});
+
+collection.items.all().forEach(printSnippet);
